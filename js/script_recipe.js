04d0a262961b6b9d88e457d82904d10afcb4d692
p-smaxh <p.smach99@gmail.com>
2020-01-21 19:31:50 +0100

    changing style
    bo brzydkie

--- js/script_recipe.js
+++ js/script_recipe.js
@@ -9,21 +9,38 @@ xmlhttp.onreadystatechange = function () {
     var opis = zap.Description;
     var przepis = zap.Instructions;
     var p = "";
-    for (i = 0; i < przepis.length; i++) {
+    for (i=0;i<przepis.length;i++){
       p = p + przepis[i];
-      if (przepis[i] == '.') {
-        p = p + "<br />";
+      if (przepis[i]=='.'){
+         p = p + "<br/>";
       }
     }
     var danie = zap.Title;
     var kuchnia = zap.Cuisine + ' - ' + zap.Category;
     var zdjecie2 = zap.ImageURL;
-
+    var skladniki = zap.Ingredients;
+    sessionStorage.setItem("liczbaKalorii", Math.round(zap.NutritionInfo.TotalCalories));
+    sessionStorage.setItem("porcja", Math.round(zap.NutritionInfo.SingularYieldUnit));
+    sessionStorage.setItem("tluszcz", Math.round(zap.NutritionInfo.TotalFat));
+    sessionStorage.setItem("wegle", Math.round(zap.NutritionInfo.TotalCarbs));
+    sessionStorage.setItem("bialko", Math.round(zap.NutritionInfo.Protein));
+    sessionStorage.setItem("sol", Math.round(zap.NutritionInfo.SatFat));
+    sessionStorage.setItem("sod", Math.round(zap.NutritionInfo.Sodium));
+    sessionStorage.setItem("cukier", Math.round(zap.NutritionInfo.Sugar));
+    var wpis = zap.Ingredients[0].Name+" - "+zap.Ingredients[0].MetricDisplayQuantity +" "+ zap.Ingredients[0].MetricUnit;
+    console.log(zap);
+    var listaSkl = document.getElementById("listaSkl");
+    for (i=0;i<zap.Ingredients.length-1;i++){
+      var punkt = document.createElement("li"); 
+      var wpis = document.createTextNode(zap.Ingredients[i].Name+" - "+zap.Ingredients[i].MetricDisplayQuantity +" "+ zap.Ingredients[i].MetricUnit);
+      punkt.appendChild(wpis); 
+      listaSkl.appendChild(punkt); 
+    }
     document.getElementById("opis").innerHTML = opis;
     document.getElementById("przepis").innerHTML = p;
     document.getElementById("danie").innerHTML = danie;
     document.getElementById("kuchnia").innerHTML = kuchnia;
-    document.getElementById("zdjecie2").src = zdjecie2
+    document.getElementById("zdjecie2").style.backgroundImage = "url("+zdjecie2+")";
   }
 };
 xmlhttp.open("GET", url, true);
--- js/script_user.js
+++ js/script_user.js
@@ -1,6 +1,6 @@
 var xmlhttp = new XMLHttpRequest();
 var url = "https://randomuser.me/api/?result=3";
-
+ 
 xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var zap = JSON.parse(this.responseText);
@@ -10,13 +10,13 @@ xmlhttp.onreadystatechange = function () {
     var zdjecie = zap.results[0].picture.large;
     var narodowosc = zap.results[0].nat;
     var wiek = zap.results[0].dob['age'];
-
+ 
     document.getElementById("dane").innerHTML = imie + " " + nazwisko;
-    document.getElementById("zdjecie").src = zdjecie;
-    document.getElementById("email").innerHTML = "<br />" + "Kontakt do twórców:" + "<br />" + email;
-    document.getElementById("nat").innerHTML = "<br />" + "Narodowość: " + narodowosc;
-    document.getElementById("dob").innerHTML = "<br />" + "Wiek: " + wiek;
-
+    document.getElementById("zdjecie").style.backgroundImage  = "url("+zdjecie+")";
+    document.getElementById("email").innerHTML = "<br />" + email;
+    document.getElementById("nat").innerHTML = "<br />" + "Nationality: " + narodowosc;
+    document.getElementById("dob").innerHTML = "<br />" + "Age: " + wiek;
+ 
   }
 };
 xmlhttp.open("GET", url, true);
